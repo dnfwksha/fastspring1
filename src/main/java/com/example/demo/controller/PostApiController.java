ef639c329989fb0c07f62700d3a3bb095a09cd90
dnfwk <dnfwksha@naver.com>
2022-04-21 06:53:18 +0900

    a

--- src/main/java/com/example/demo/controller/PostApiController.java
+++ src/main/java/com/example/demo/controller/PostApiController.java
@@ -4,12 +4,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api")
 public class PostApiController {
 
     @PostMapping("/post")
-    public void post(){
+    public void post(Map<String,Object> requestData){
+
+        requestData.forEach((key, value) -> {
+            System.out.println("key : " + key);
+            System.out.println("value : " + value);
+        });
 
     }
 }
